59c76930ea3746e037b57324070fa2478c68b54f
yan_sy <yan_sy@126.com>
2018-07-30 14:19:25 +0800

    init dfs() and performAction() method

--- autoPlay_3.py
+++ autoPlay_3.py
@@ -11,7 +11,21 @@ def init(pause=1):
             'autodump': False, 'debug': {}, 'startviewserver': True, 'compresseddump': True}
     vc = ViewClient(device, serialno, **kwargs2)
     return vc
+
+def dfs(parentView):
+    performAction(parentView)
+    for view in parentView.getChildren():
+        dfs(view)
+
+def performAction(view):
+    print view.getClass()
+    print view.getText()
+    print view.getCenter()
+    print '\n'
     
 
 if __name__ == '__main__':
     vc = init(1)
+    vc.dump()
+    rootView = vc.getRoot()
+    dfs(rootView)
